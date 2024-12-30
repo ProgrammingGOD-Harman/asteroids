9feebdd25d486c6f60fb44d6c8a87d332b0e3844
Harman Deep Singh <harmanthebest@gmail.com>
2024-12-30 00:31:08 -0500

    Added movement to the ship (player)

--- main.py
+++ main.py
@@ -8,8 +8,16 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock =  pygame.time.Clock() # Big bang the start of time
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+
     dt = 0
+
 
 
     # GAME LOOP
@@ -18,8 +26,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
 
+        for obj in updatable:
+            obj.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
